000540c543de29caf29a4eb1a7e3abd90ef30760
andrejmanin <andrejmanin54@gmail.com>
2025-07-01 00:45:51 +0300

    Added new function - creating posts. Fixed bugs.

--- Client/Services/PostService.cs
+++ Client/Services/PostService.cs
@@ -1,6 +1,7 @@
 using System.Net.Http.Json;
 using System.Collections.ObjectModel;
 using Client.Models.DTO.PostDtos;
+using Client.ViewModels;
 
 namespace Client.Services;
 
@@ -11,5 +12,18 @@ public class PostService
         string url = "api/posts/get/all";
         var posts = await HttpClientBase.HttpClient.GetFromJsonAsync<ObservableCollection<PostDto>>(url);
         return posts ??  new ObservableCollection<PostDto>();
+    }
+
+    public async Task CreatePost(CreatePostDto post)
+    {
+        string url = "api/posts/create";
+
+        var response = await HttpClientBase.HttpClient.PostAsJsonAsync(url, post);
+
+        if (!response.IsSuccessStatusCode)
+        {
+            var errorContent = await response.Content.ReadAsStringAsync();
+            throw new Exception($"Error creating post: {response.StatusCode}, {errorContent}");
+        }
     }
 }
